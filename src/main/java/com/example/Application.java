755133ef2adbed3ad4c6363c063f3a7ab493b69b
ratheesh <anjanakukkutp@gmail.com>
2023-11-06 20:21:42 +0530

    print

--- src/main/java/com/example/Application.java
+++ src/main/java/com/example/Application.java
@@ -5,6 +5,7 @@ import io.micronaut.runtime.Micronaut;
 public class Application {
 
     public static void main(String[] args) {
+        System.out.println("hi");
         Micronaut.run(Application.class, args);
     }
 }
